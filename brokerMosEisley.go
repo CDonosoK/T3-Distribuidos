bf308492023e3e2e0d64b942b83b56920071b786
CDonosoK <clemente.donosok@gmail.com>
2021-12-04 22:12:02 -0300

    actualización conexiones broker: informantes, leia, servidores

--- brokerMosEisley.go
+++ brokerMosEisley.go
@@ -11,19 +11,54 @@ import (
 )
 
 func main(){
-	lis, err := net.Listen("tcp", ":9000")
-	if err != nil {
-		log.Fatalf("Failed to listen on port 9000: %v", err)
+	//Conexión broker - informantes
+	lis0, err0 := net.Listen("tcp", ":9000")
+	if err0 != nil {
+		log.Fatalf("Failed to listen on port 9000: %v", err0)
 	}
 
-	s := chat.Server{}
+	s0 := chat.Server{}
 
-	grpcServer := grpc.NewServer()
+	grpcServer0 := grpc.NewServer()
 
-	chat.RegisterChatServer(grpcServer, &s)
+	chat.RegisterChatServer(grpcServer0, &s0)
 
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+	if err0 := grpcServer0.Serve(lis0); err0 != nil {
+		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err0)
+
+	}
+
+	//Conexión broker - leia
+	lis1, err1 := net.Listen("tcp", ":9001")
+	if err1 != nil {
+		log.Fatalf("Failed to listen on port 9001: %v", err1)
+	}
+
+	s1 := chat.Server{}
+
+	grpcServer1 := grpc.NewServer()
+
+	chat.RegisterChatServer(grpcServer1, &s1)
+
+	if err1 := grpcServer1.Serve(lis1); err1 != nil {
+		log.Fatalf("Failed to serve gRPC server over port 8000: %v", err1)
+
+	}
+
+	//Conexión broker - servidores
+	lis2, err2 := net.Listen("tcp", ":9002")
+	if err2 != nil {
+		log.Fatalf("Failed to listen on port 8000: %v", err2)
+	}
+
+	s2 := chat.Server{}
+
+	grpcServer2 := grpc.NewServer()
+
+	chat.RegisterChatServer(grpcServer2, &s2)
+
+	if err2 := grpcServer2.Serve(lis2); err2 != nil {
+		log.Fatalf("Failed to serve gRPC server over port 8000: %v", err2)
 
 	}
 
